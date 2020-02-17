06979511d9deeca628be4f749ccc92231c992ef8
Egor Dorichev <egordorichev@gmail.com>
2020-02-17 17:11:43 +0300

    Implemented bk:MeleeArc and bk:SimpleShoot uses rendering

--- uses.renderer.js
+++ uses.renderer.js
@@ -1,3 +1,19 @@
+var renderers = {};
+
+/**
+ * Renders a single item use
+ * @param {Array<any>} use The uses
+ * @returns {string} The rendered HTML
+ */
+function renderUse(use) {
+  var renderer = renderers[use.id];
+
+  if (renderer) {
+    return renderer(use);
+  }
+
+  return use.id;
+}
 
 /**
  * Render and item's uses property to HTML
@@ -5,6 +21,24 @@
  * @returns {string} The rendered HTML
  */
 function renderUses(uses) {
-  // TODO do your thing egor :p
-  return uses.map(u => u.id).join(', ');
+  var result = [];
+
+  for (var use in uses) {
+    var data = renderUse(uses[use]);
+
+    if (data) {
+      result.push("\n* ");
+      result.push(data);
+    }
+  }
+
+  return mdToHtml(result.join(""));
+}
+
+renderers["bk:MeleeArc"] = function(use) {
+  return `Creates a melee arc, that does ${use.damage} damage and lasts for ${use.time} seconds`
+}
+
+renderers["bk:SimpleShoot"] = function(use) {
+  return `Shoots ${use.amount == 1 ? "a projectile, that deals " : `${use.amount} projectiles, that deal `} ${use.damage} damage`
 }
